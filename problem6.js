d160c70cd6bb6096265ad9001b500b2ef6298835
SenpaiKishore <108696612+SenpaiKishore@users.noreply.github.com>
2024-05-02 13:59:32 +0530

    Problem 6 solved. Getting the complete car object which has car make BMW and Audi from the inventory array

---
+++ problem6.js
@@ -0,0 +1,14 @@
+// ==== Problem #6 ====
+// A buyer is interested in seeing only BMW and Audi cars within the inventory.  Execute a function and return an array that only contains BMW and Audi cars.  Once you have the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+
+function problem6(inventory){
+    const filterdCars = []
+    for(let i = 0; i < inventory.length; i++){
+        if(inventory[i].car_make == "BMW" || inventory[i].car_make == "Audi"){
+            filterdCars.push(inventory[i]);
+        }      
+    }
+    return JSON.stringify(filterdCars);
+}
+
+module.exports.problem6 = problem6;
